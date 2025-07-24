2826d24f1830d0793aaa6b65488f2a8c480a690e
Roshan <roshan18@Roshans-MacBook-Air.local>
2025-07-24 16:54:04 +0530

    Fetched User Data By Github Api

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,21 @@
 import { address } from "motion/react-client";
 import User from "./User.js";
 import UserClass from "./UserClass.js";
+import { Component } from "react";
 
-const About = () => {
-  return (
-    <div>
-      <h1>About Us</h1>
-      <User name={"Roshan Function"} address={"Birpur"} />
-      <UserClass name={"Roshan Class"} address={"Noida"} />
-    </div>
-  );
-};
+class About extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>About Us</h1>
+        <UserClass />
+      </div>
+    );
+  }
+}
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -6,18 +6,27 @@ class UserClass extends React.Component {
     super(props);
 
     this.state = {
-      count: 0,
+      userInfo: {
+        name: "Demo",
+        location: "Default",
+      },
     };
+  }
+  async componentDidMount() {
+    const data = await fetch("https://api.github.com/users/RoshnV18");
+    const json = await data.json();
+
+    this.setState({
+      userInfo: json,
+    });
   }
 
   render() {
-    const { count } = this.state;
-    const { name, address } = this.props;
+    const { name, location } = this.state.userInfo;
     return (
       <div>
-        <h1>{count}</h1>
         <h3>Name : {name}</h3>
-        <h3>Location : {address}</h3>
+        <h3>Location :{location}</h3>
       </div>
     );
   }
